d3620ed0f15d9c2be58280c8a36f9c8e443f5e97
faizachwd <faiza.chowdhury789@gmail.com>
2025-02-12 17:13:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 mapboxgl.accessToken = "pk.eyJ1IjoiZmFpemExMzIiLCJhIjoiY201d3E1Y2JwMDByYzJrb290MWltMTN1dyJ9.JsEiKVuT3vMCT8JQDlDA4g";
-const map = new mapboxgl.Map ({
-container: "my-map",
-style:'mapbox://styles/faiza132/cm72f283a007a01quayc2g04v',
-center:[-79.3878583,43.7205208],
-zoom: 12
+const map = new mapboxgl.Map({
+    container: "my-map",
+    style: 'mapbox://styles/faiza132/cm72f283a007a01quayc2g04v',
+    center: [-79.3878583, 43.7205208],
+    zoom: 12
 });
 
-map.on("load", ()=>{
+map.on("load", () => {
     map.addSource('EarlyOn', {
         type: 'geojson',
-        data: "https://github.com/faizachwd/GGR472-Lab-2/blob/61ae09a5b75f0da0815e9cf1e51384d916400f25/EarlyON%20Child%20and%20Family%20Centres%20Locations%20-%20geometry%20-%204326.geojson"
+        data: "https://raw.githubusercontent.com/faizachwd/GGR472-Lab-2/main/EarlyON Child and Family Centres Locations - geometry - 4326.geojson"
     });
     map.addLayer({
-    id: "EarlyOn_pts",
-    type: "circle",
-    source:'EarlyOn'   
+        id: "EarlyOn_pts",
+        type: "circle",
+        source: 'EarlyOn',
+        paint: {
+            'circle-radius': 4,
+            'circle-color': 'hotpink'
+        }
     })
-})
-
-// questions:
-// do we add our data onto the style? 
-// do we need to add one dataset as a geojson and one as a tileset? 
+    map.addSource('Schools_tile', {
+        type: "vector",
+        url: 'mapbox://faiza132.262ld0bt',
+    });
+    map.addLayer({
+        id: 'School-pts',
+        type: 'circle',
+        source: 'Schools_tile',
+        paint: {
+            'circle-radius': 6,
+            'circle-color': 'blue',
+            'circle-opacity': 0.4
+        },
+        'source-layer': "School_locations-all_types_da-0fuese"
+    });
+}, "School-pts"
+)
